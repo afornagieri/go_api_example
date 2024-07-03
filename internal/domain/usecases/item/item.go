8fc3afe16e92960133bef512ac7f1c32599f4d15
forna <fornagieri.artur@gmail.com>
2024-07-03 20:48:30 -0300

    implements methods CreateItem, UpdateItem and DeleteItem

--- internal/domain/usecases/item/item.go
+++ internal/domain/usecases/item/item.go
@@ -1,6 +1,8 @@
-package entities
+package usecases
 
 import (
+	"errors"
+
 	entities "github.com/afornagieri/go_api_template/internal/domain/entities/item"
 	repositories "github.com/afornagieri/go_api_template/internal/infra/repositories"
 )
@@ -30,5 +32,34 @@ func (uc *ItemUseCase) GetItemByName(name string) (*entities.Item, error) {
 }
 
 func (uc *ItemUseCase) CreateItem(item entities.Item) error {
+	existingItem, err := uc.Repo.GetItemByName(item.Name)
+	if err != nil && err.Error() != "item not found" {
+		return err
+	}
+	if existingItem != nil {
+		return errors.New("item already exists")
+	}
 	return uc.Repo.CreateItem(item)
+}
+
+func (uc *ItemUseCase) UpdateItem(name string, item entities.Item) error {
+	existingItem, err := uc.Repo.GetItemByName(name)
+	if err != nil {
+		return err
+	}
+	if existingItem == nil {
+		return errors.New("item does not exist")
+	}
+	return uc.Repo.UpdateItem(name, item)
+}
+
+func (uc *ItemUseCase) DeleteItem(name string) error {
+	existingItem, err := uc.Repo.GetItemByName(name)
+	if err != nil {
+		return err
+	}
+	if existingItem != nil {
+		return errors.New("item does not exist")
+	}
+	return uc.Repo.DeleteItem(name)
 }
