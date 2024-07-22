b02f9c3677d0d9397df878d94c12e1902c1a00e6
forna <fornagieri.artur@gmail.com>
2024-07-22 19:44:09 -0300

    add fields to control error behavior for GetItems, GetItemByName, CreateItem, UpdateItem, and DeleteItem methods

--- tests/unit_tests/mocks/item_repository_mock.go
+++ tests/unit_tests/mocks/item_repository_mock.go
@@ -7,7 +7,12 @@ import (
 )
 
 type MockItemRepository struct {
-	items map[string]*entities.Item
+	items                 map[string]*entities.Item
+	shouldErrorGetItems   bool
+	shouldErrorGetItem    bool
+	shouldErrorCreateItem bool
+	shouldErrorUpdateItem bool
+	shouldErrorDeleteItem bool
 }
 
 func NewMockItemRepository() *MockItemRepository {
@@ -16,7 +21,25 @@ func NewMockItemRepository() *MockItemRepository {
 	}
 }
 
+func (m *MockItemRepository) SetError(method string, shouldError bool) {
+	switch method {
+	case "GetItems":
+		m.shouldErrorGetItems = shouldError
+	case "GetItem":
+		m.shouldErrorGetItem = shouldError
+	case "CreateItem":
+		m.shouldErrorCreateItem = shouldError
+	case "UpdateItem":
+		m.shouldErrorUpdateItem = shouldError
+	case "DeleteItem":
+		m.shouldErrorDeleteItem = shouldError
+	}
+}
+
 func (m *MockItemRepository) GetItems() ([]*entities.Item, error) {
+	if m.shouldErrorGetItems {
+		return nil, errors.New("internal server error")
+	}
 	var itemList []*entities.Item
 	for _, itm := range m.items {
 		itemList = append(itemList, itm)
@@ -25,6 +48,9 @@ func (m *MockItemRepository) GetItems() ([]*entities.Item, error) {
 }
 
 func (m *MockItemRepository) GetItemByName(name string) (*entities.Item, error) {
+	if m.shouldErrorGetItem {
+		return nil, errors.New("internal server error")
+	}
 	itm, exists := m.items[name]
 	if !exists {
 		return nil, errors.New("item not found")
@@ -33,6 +59,9 @@ func (m *MockItemRepository) GetItemByName(name string) (*entities.Item, error) 
 }
 
 func (m *MockItemRepository) CreateItem(itm *entities.Item) error {
+	if m.shouldErrorCreateItem {
+		return errors.New("internal server error")
+	}
 	if _, exists := m.items[itm.Name]; exists {
 		return errors.New("item already exists")
 	}
@@ -41,6 +70,9 @@ func (m *MockItemRepository) CreateItem(itm *entities.Item) error {
 }
 
 func (m *MockItemRepository) UpdateItem(name string, itm *entities.Item) error {
+	if m.shouldErrorUpdateItem {
+		return errors.New("internal server error")
+	}
 	if _, exists := m.items[name]; !exists {
 		return errors.New("item not found")
 	}
@@ -49,6 +81,9 @@ func (m *MockItemRepository) UpdateItem(name string, itm *entities.Item) error {
 }
 
 func (m *MockItemRepository) DeleteItem(name string) error {
+	if m.shouldErrorDeleteItem {
+		return errors.New("internal server error")
+	}
 	if _, exists := m.items[name]; !exists {
 		return errors.New("item not found")
 	}
